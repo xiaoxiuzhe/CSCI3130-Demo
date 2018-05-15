7561bc8d4962563085772a6235f022ec1bc67518
XIUZHE\xiuzhe <xiaoxiuzhe@163.com>
2018-05-15 15:00:56 -0300

    added a button and a text field that can copy the text

--- app/src/main/java/com/example/acme/csci3130_demo/MainActivity.java
+++ app/src/main/java/com/example/acme/csci3130_demo/MainActivity.java
@@ -2,6 +2,9 @@ package com.example.acme.csci3130_demo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void buttonOnClick(View v){
+        TextView textView = (TextView) findViewById(R.id.textView4);
+        EditText editText = (EditText) findViewById(R.id.editText);
+        textView.setText(editText.getText());
     }
 }
